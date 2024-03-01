f3301b96e5e70f940eeaa6f640086bfc8da1d744
Hamdi Adan <hhhaji013@gmail.com>
2024-03-01 21:13:46 +0300

    completed

--- lib/looping.py
+++ lib/looping.py
@@ -2,12 +2,27 @@
 
 def happy_new_year():
     # code goes here!
-    pass
+    for i in range(10, 0,-1):
+        print(i)
+    print("Happy New Year!")    
+    
 
 def square_integers(int_list):
     # code goes here!
-    pass
+    square_integers=[x ** 2 for x in int_list]
+    return square_integers
+
 
 def fizzbuzz():
     # code goes here!
-    pass
+    for i in range(1,101):
+        if i % 3==0 and i%5==0:
+            print("FizzBuzz")
+        elif i%3==0:
+            print("Fizz")
+        elif i%5==0:
+            print("Buzz")    
+
+        else:
+            print(i)        
+    
